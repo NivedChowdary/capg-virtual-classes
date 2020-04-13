ecfadacf8ef93289660f079bd9dadd89b1172da2
Nived Chowdary <nivedchoudary@gmail.com>
2020-04-13 09:31:23 +0530

    Rating

---
+++ capg-movie-rating/src/main/java/com/capg/movierating/model/MovieRating.java
@@ -0,0 +1,24 @@
+package com.capg.movierating.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MovieRating {
+	
+	@Id
+	private long id;
+	private double rating;
+
+}
